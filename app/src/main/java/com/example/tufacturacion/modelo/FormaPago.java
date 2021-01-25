a49b4ba9ea98d62fb2267ee559524c829c0f5c05
Manuel Jesús Rodriguez Arabi <manupb@gmail.com>
2021-01-25 10:48:04 +0100

    Creación manejador SQLiteOpenHelper y Modelos

---
+++ app/src/main/java/com/example/tufacturacion/modelo/FormaPago.java
@@ -0,0 +1,12 @@
+package com.example.tufacturacion.modelo;
+
+public class FormaPago {
+    public String idFormaPago;
+
+    public String nombre;
+
+    public FormaPago(String idFormaPago, String nombre) {
+        this.idFormaPago = idFormaPago;
+        this.nombre = nombre;
+    }
+}
---
+++ app/src/main/java/com/example/tufacturacion/modelo/Producto.java
@@ -0,0 +1,18 @@
+package com.example.tufacturacion.modelo;
+
+public class Producto {
+    public String idProducto;
+
+    public String nombre;
+
+    public float precio;
+
+    public int existencias;
+
+    public Producto(String idProducto, String nombre, float precio, int existencias) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.existencias = existencias;
+    }
+}
